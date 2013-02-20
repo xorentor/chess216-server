6d03c6a7bb53a14d4205d4a1e332b084fb11cc86
xorentor <xorentor@pi4.(none)>
2013-02-20 01:29:56 +0000

    common.h

--- common.h
+++ common.h
@@ -11,23 +11,16 @@
 #include <time.h>
 #include <pthread.h>
 #include <memory.h>
-#include <stdbool.h>
 
 #define 	_DEBUG
 #define		INLINE		inline
 
-typedef struct Packet_s
-	{
-	bool continuous;
-	int command;
-	int datalen;
-	char data[256];
-} Packet_t;
+#define		F_QUIT		0x0001
 
 typedef struct Thread_s
 {
 	pthread_t pthread;
-	bool inuse;
+	int inuse;
 	time_t spawned;
 } Thread_t;
 
@@ -38,10 +31,38 @@ typedef struct ThreadParam_s
 		
 } ThreadParam_t;
 
-typedef struct Client_s
+typedef struct PacketData_s
 {
-	int socketDesc;
-	bool inuse;
-} Client_t;	
+	char command;
+	char length;	
+	char data[ 225 ];
+} PacketData_t;
+
+INLINE int       asm_strcmp( const char *s, const char *d, const int c )
+{
+        int r;
+        __asm__
+        (
+                "testl  %%eax, %%eax;"
+                "jz     .cmpf;"
+                "testl  %%ebx, %%ebx;"
+                "jz     .cmpf;"
+                "mov    %%eax, %%esi;"
+                "mov    %%ebx, %%edi;"
+                "cld;"
+                "repe   cmpsb;"
+                "je     .cmpt;"
+                ".cmpf:;"
+                "movl   $1, %%eax;"
+                "jmp    .cmpend;"
+                ".cmpt:;"
+                "xorl   %%eax, %%eax;"
+                ".cmpend:;"
+                : "=a"(r)
+                : "a"(s), "b"(d), "c"(c)
+        );
+        return r;
+}
+
 
 #endif
